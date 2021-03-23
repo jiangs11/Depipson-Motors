6160e3e55a2b22ab1ca256015bc3dbee915984f3
Steven Jiang <jiangs1@students.rowan.edu>
2021-03-23 18:35:25 -0400

    still fixing deploy api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 app.get('/', (req,res) => {
-    res.send('Welcome to the Depipson Motors API!')
+    res.send('Welcome to the Depipson Motors API! Update: 6:35 PM')
 })
 
 // Note to self: jiangsteven99@gmail.com Mongo Atlas account
@@ -19,8 +19,8 @@ const connection = mongoose.connection
 
 connection.once('open', () => {
     console.log("MongoDB database connection established successfully")
+})
 
-    app.listen(process.env.PORT || 5000, () => {
-        console.log(`Server is running on port: ${process.env.PORT}`)
-    })
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server is running on port: ${process.env.PORT}`)
 })
