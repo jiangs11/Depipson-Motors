cc576ece68e58690cefab7f3a3db0f7aaf5cabc6
Elija Amponsah <amponsaeli@gmail.com>
2021-04-02 23:17:27 -0400

    Added carousel

---
+++ app/src/components/Jumbotron.js
@@ -0,0 +1,55 @@
+ import Carousel from 'react-bootstrap/Carousel'
+import image1 from './../statics/images/image1.jpg'
+import React, { useState } from 'react'
+
+function Jumbotron() {
+    const [index, setIndex] = useState(0);
+  
+    const handleSelect = (selectedIndex, e) => {
+      setIndex(selectedIndex);
+    };
+  
+    return (
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            source={image1}  
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            source={image1}
+            alt="Second slide"
+          />
+  
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            source={image1}
+            alt="Third slide"
+          />
+  
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
+  }
+  
+  export default Jumbotron
--- app/src/pages/LandingPage.js
+++ app/src/pages/LandingPage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../components/Header'
 import { withRouter } from 'react-router-dom'
+import Jumbotron from '../components/Jumbotron'
 
 function LandingPage({ history }) {
     return (
         <div>
             <Header />
+            <Jumbotron />
             <h1>Landing Page</h1>
             <button onClick={() => history.push('car')}>Go to Car Page</button>
         </div>
