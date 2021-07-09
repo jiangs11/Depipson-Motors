2be1292c3343fb2fa73aa1dea76de5944bcdfee4
Elija Amponsa <amponseli@gmail.com>
2021-07-08 23:16:47 -0400

    Update

--- app/src/pages/admin/AdminDashboard.js
+++ app/src/pages/admin/AdminDashboard.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Loadfile from '../../components/Loadfile'
 import InfoTable from '../../components/InfoTable'
 import InfoTable2 from '../../components/InfoTable2'
-import { Form } from 'react-bootstrap'
+import { Form, Button } from 'react-bootstrap'
 
 function AdminDashboard() {
     return (
         <div>
             {/* <AdminSideNav /> */}
-            <h1 style={{textAlign:"center"}}>VIN DECODER & LookUp</h1>
+            <h1 style={{textAlign:"center", }}>VIN DECODER & LookUp</h1>
             <div style={{textAlign:"center"}}>Decode Your Vehicle Identification Number Here</div>
             <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
                 <Form.Group style={{ width: '50%' }}>
@@ -18,7 +18,12 @@ function AdminDashboard() {
             <InfoTable />
             <InfoTable2 />
             <Loadfile />
-    </div>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+                <div className="mb-2">
+                    <Button variant="primary" size="lg">Submit</Button>
+                </div>
+            </div>
+        </div>
     )
 }
 
